6ffc63d418075042baf852aa3f631d146a561a78
dx <dx@devextreme-mini-server>
2020-09-24 10:31:05 +0300

    Update script to sync demos on github repo

--- rootfs/syncer.sh
+++ rootfs/syncer.sh
@@ -38,6 +38,10 @@ while true; do
         asp_demos_path=/repos/$branch/asp-demos
         wg_external_path=$hg_path/Demos/WidgetsGallery/ExternalDemoSources
 
+        demos_on_github_path=/repos/$branch/demos-on-github
+        demos_on_github_hg_path=$hg_path/Demos/WidgetsGallery/WidgetsGallery
+        wg_mvc_demos_path=$hg_path/Demos/WidgetsGallery/WidgetsGallery.MVC
+
         crossplatform_core_path=/repos/$branch/crossplatform-core
         data_portions_path=$hg_path/Tools/DevExpress.Data.Portions
 
@@ -52,8 +56,13 @@ while true; do
             || echo "Sync from GitHub failed"
 
         if [ -d $asp_demos_path ]; then
+            if [[ $branch > "20_1"  ]]; then
+                demo_shell_path=$hg_path/Demos/WidgetsGallery/AspNetCoreDemos.DemoShell
+            else
+                demo_shell_path=$hg_path/Demos/WidgetsGallery/WidgetsGallery.MVC/DevExtreme.NETCore.Demos
+            fi
             /git-update.sh $asp_demos_path 20${branch/_/.} $asp_demos_path.log \
-                && /rsync-multi.sh $asp_demos_path/AspNetCoreDemos.DemoShell $hg_path/Demos/WidgetsGallery/WidgetsGallery.MVC/DevExtreme.NETCore.Demos DemoShell/ wwwroot/DemoShell/ .editorconfig \
+                && /rsync-multi.sh $asp_demos_path/AspNetCoreDemos.DemoShell $demo_shell_path DemoShell/ wwwroot/DemoShell/ .editorconfig \
                 && /rsync-multi.sh $asp_demos_path $wg_external_path AspNetCoreDemos.Reporting/ AspNetCoreDemos.RichEdit/ AspNetCoreDemos.Spreadsheet/ \
                 && find $wg_external_path -type f -regextype posix-egrep -not -regex ".*(README|menuMeta\.json|DemosStyles.*css|DemosScripts.*js|\.(cs|cshtml|md))$" -delete \
                 && /hg-commit.sh $hg_path $asp_demos_path.log \
@@ -65,6 +74,15 @@ while true; do
             && /rsync-multi.sh $crossplatform_core_path/Win/DevExpress.Data/DevExpress.Data $data_portions_path AssemblyVersion.cs Utils/Logify.cs Utils/UAlgo.cs Utils/UAlgoConstants.cs Utils/UAlgoPost.cs Utils/UData.cs Utils/UTest.cs \
             && /hg-commit.sh $hg_path $crossplatform_core_path.log \
             || echo "Sync from CrossPlatform/Core failed"
+        fi
+
+        if [ -d $demos_on_github_path ]; then
+            /git-update.sh $demos_on_github_path $branch $demos_on_github_path.log \
+            && /rsync-multi.sh $demos_on_github_path $demos_on_github_hg_path utils/ JSDemos/ package.json \
+            && /rsync-multi.sh $demos_on_github_path/MVCDemos $wg_mvc_demos_path/DevExtreme.MVC.Demos / \
+            && /rsync-multi.sh $demos_on_github_path/NetCoreDemos $wg_mvc_demos_path/DevExtreme.NETCore.Demos / \
+            && /hg-commit.sh $hg_path $demos_on_github_path.log \
+            || echo "Sync from Demos/WidgetsGallery/WidgetsGallery failed"
         fi
 
         if ! /hg-push.sh $hg_path $branch; then
