c6c16b6455fc4dd5f7047e881cddbf9b017ccc14
Alexey Serov <alexserov@users.noreply.github.com>
2021-11-12 11:22:49 +0300

    allow ignoring non-existent branches

--- rootfs/git-update.sh
+++ rootfs/git-update.sh
@@ -15,11 +15,19 @@ if [ -f .git/index.lock ] && ! pidof git; then
     unlink .git/index.lock
 fi
 
-git fetch --force --depth=100 --no-tags origin $BRANCH:$BRANCH || exit 1
+echo "syncing $REPO_PATH $BRANCH $LOG_PATH"
+remote_url=$(git remote -v | grep -Po '(?<=origin\s).*(?=\s\(fetch\))' | head -n 1)
 
-git log --pretty=format:"%h %an - %s" HEAD..FETCH_HEAD > "$LOG_PATH"
-if [ ! -s "$LOG_PATH" ]; then
-    echo "(empty log)" > "$LOG_PATH"
+if [ "$(git ls-remote --heads $remote_url $BRANCH | wc -l)" == "1" ]; then
+
+    git fetch --force --depth=100 --no-tags origin $BRANCH:$BRANCH || exit 1
+
+    git log --pretty=format:"%h %an - %s" HEAD..FETCH_HEAD > "$LOG_PATH"
+    if [ ! -s "$LOG_PATH" ]; then
+        echo "(empty log)" > "$LOG_PATH"
+    fi
+
+    git checkout -qf FETCH_HEAD
+else
+    echo "The $BRANCH does not exist on $remote_url"
 fi
-
-git checkout -qf FETCH_HEAD
